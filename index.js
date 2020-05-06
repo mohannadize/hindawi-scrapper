e176184daabaf02caac86cab9ba3abf1e0f8f17e
Mohannad Hisham <mohannadize15@gmail.com>
2020-05-07 01:21:29 +0200

    minor type

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const fetch = require("node-fetch");
 const { fetch_database } = require("./fetch_database");
 const fs = require("fs");
