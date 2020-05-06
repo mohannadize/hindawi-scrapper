ead307582cbd1ff61e6bdcdce575494ca06a4a9b
Mohannad Hisham <mohannadize15@gmail.com>
2020-05-07 00:40:18 +0200

    added category request

--- index.js
+++ index.js
@@ -16,16 +16,33 @@ fs.readFile("./database/database.json", (err, buffer) => {
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 app.get("/", (req, res) => {
-    let params = new URLSearchParams(req.query);
     let books;
-    let search = params.get('search');
+    let search = req.params.search || false;
     if (search) {
         books = db_array.filter(book => {
-            return book.author.indexOf(search) != -1;
+            return book.author.indexOf(search) != -1 || book.title.indexOf(search) != -1 || book.description.indexOf(search) != -1;
         }).slice(0, 10);
     } else {
         let random_offset = Math.abs(Math.floor(Math.random() * db_array.length) - 10)
         books = db_array.slice(random_offset, random_offset + 10);
+    }
+
+
+    res.render('index', { books, search })
+});
+
+app.get("/category/*", (req, res) => {
+    let category = req.url.match(/category\/([^\/|.]+)\/*(\d)*\/*/i)[1];
+    let page = req.url.match(/category\/([^\/|.]+)\/*(\d)*\/*/i)[2] || 1;
+    let books = db_array.filter(book => book.subject.name == category);
+    let search = req.params.search || false;
+    if (search) {
+        books = books.filter(book => {
+            return book.author.indexOf(search) != -1 || book.title.indexOf(search) != -1 || book.description.indexOf(search) != -1;
+        }).slice(0, 10);
+    } else {
+        let random_offset = Math.abs(Math.floor(Math.random() * books.length) - 10)
+        books = books.slice(random_offset, random_offset + 10);
     }
 
 
